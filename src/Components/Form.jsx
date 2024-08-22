a5950ae9d1d583a4dbe15175bc441c4957fe6ba1
zepher-23 <30zepher@gmail.com>
2024-08-22 10:50:59 +0530

    added forms

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -46,22 +46,31 @@ const Form = () => {
     
       return (
         <div className="max-w-md mx-3 lg:mx-6 mt-28 my-10 p-8 bg-gray-100 bg-opacity-20 shadow-md">
-         <h4 className="text-3xl font-bold text-white text-center mb-6">Discover the perfect diet and exercise plan to reach your peak fitness!</h4>
+          <h4 className="text-3xl font-bold text-white text-center mb-6">
+            Discover the perfect diet and exercise plan to reach your peak
+            fitness!
+          </h4>
 
-          <h4 className="text-sm font-light text-left mb-2">To give you the best possible insights, we just need a little info from you!</h4>
-    
+          <h4 className="text-sm font-light text-left mb-2">
+            To give you the best possible insights, we just need a little info
+            from you!
+          </h4>
+
           <div className="mb-4 flex items-center ">
             <button
               onClick={toggleUnits}
               className="px-4 py-1 bg-opacity-40 bg-primary text-white text-md"
             >
-               {formData.units === 'metric' ? 'Imperial' : 'Metric'} 
+              {formData.units === "metric" ? "Imperial" : "Metric"}
             </button>
-            <h5 className="mx-2 text-sm font-light text-left">Click here to change measurement unit.
-</h5>
+            <h5 className="mx-2 text-sm font-light text-left">
+              Click here to change measurement unit.
+            </h5>
           </div>
           <div className="mb-4">
-            <label className="block text-lg mb-2 text-left px-2 mt-6">Name</label>
+            <label className="block text-lg mb-2 text-left px-2 mt-6">
+              Name
+            </label>
             <input
               type="text"
               name="name"
@@ -71,7 +80,7 @@ const Form = () => {
               placeholder="Enter your name"
             />
           </div>
-    
+
           <div className="mb-4">
             <label className="block text-lg mb-2 text-left px-2">Age</label>
             <input
@@ -83,9 +92,11 @@ const Form = () => {
               placeholder="Enter your age"
             />
           </div>
-    
+
           <div className="mb-4">
-            <label className="block text-lg mb-2 text-left px-2" >Height ({heightPlaceholder})</label>
+            <label className="block text-lg mb-2 text-left px-2">
+              Height ({heightPlaceholder})
+            </label>
             <input
               type="number"
               name="height"
@@ -95,9 +106,11 @@ const Form = () => {
               placeholder={`Enter your height in ${heightPlaceholder}`}
             />
           </div>
-    
+
           <div className="mb-4">
-            <label className="block text-lg mb-2 text-left px-2">Weight ({weightPlaceholder})</label>
+            <label className="block text-lg mb-2 text-left px-2">
+              Weight ({weightPlaceholder})
+            </label>
             <input
               type="number"
               name="weight"
@@ -109,42 +122,27 @@ const Form = () => {
           </div>
 
           <div className="mb-4">
-            <label className="block text-lg mb-2 text-left px-2">Goals </label>
-            <input
-              type="text"
-              name="injury"
-              value={formData.goal}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border-b-2  outline-none focus:outline-none rounded-lg bg-transparent"
-              placeholder="What is your fitness goal?"
-            />
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-lg mb-2 text-left px-2">Injuries/Limitations </label>
-            <input
-              type="text"
-              name="injury"
-              value={formData.injury}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border-b-2  outline-none focus:outline-none rounded-lg bg-transparent"
-              placeholder="Do you have any physical limitations?"
-            />
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-lg mb-2 text-left px-2">Diet Preference </label>
-            <input
-              type="text"
-              name="injury"
+            <label className="block text-lg mb-2 text-left px-2">
+              Diet Preference
+            </label>
+            <select
+              name="dietPref"
               value={formData.dietPref}
               onChange={handleChange}
-              className="w-full px-3 py-2 border-b-2  outline-none focus:outline-none rounded-lg bg-transparent"
-              placeholder="What is your diet preference?"
-            />
+              className="w-full px-3 py-2 border-b-2 outline-none focus:outline-none rounded-lg bg-transparent"
+            >
+              <option value="" className="text-black text-sm"  disabled>
+                Select your diet preference
+              </option>
+              <option className="text-black" value="Vegetarian">Vegetarian</option>
+              <option className="text-black" value="Non-Vegetarian">Non-Vegetarian</option>
+              <option className="text-black" value="Both">Both</option>
+            </select>
           </div>
           <div className="mb-4">
-            <label className="block text-lg mb-2 text-left px-2">Allergies </label>
+            <label className="block text-lg mb-2 text-left px-2">
+              Allergies{" "}
+            </label>
             <input
               type="text"
               name="injury"
@@ -156,29 +154,93 @@ const Form = () => {
           </div>
 
           <div className="mb-4">
-  <label className="block text-lg mb-2 text-left px-2">Current Activity Level</label>
+            <label className="block text-lg mb-2 text-left px-2">
+              Injuries/Limitations{" "}
+            </label>
+            <input
+              type="text"
+              name="injury"
+              value={formData.injury}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border-b-2  outline-none focus:outline-none rounded-lg bg-transparent"
+              placeholder="Do you have any physical limitations?"
+            />
+          </div>
+
+          <div className="mb-4">
+  <label className="block text-lg mb-2 text-left px-2">Goals</label>
   <select
-    name="curActivity"
-    value={formData.curActivity}
+    name="goal"
+    value={formData.goal}
     onChange={handleChange}
     className="w-full px-3 py-2 border-b-2 outline-none focus:outline-none rounded-lg bg-transparent"
   >
-    <option className="text-primary text-sm" value="" disabled>Select your activity level</option>
-    <option className="text-black" value="Sedentary (little or no exercise)">Sedentary (little or no exercise)</option>
-    <option className="text-black" value="Lightly active (light exercise/sports 1-3 days/week)">Lightly active (light exercise/sports 1-3 days/week)</option>
-    <option className="text-black" value="Moderately active (moderate exercise/sports 3-5 days/week)">Moderately active (moderate exercise/sports 3-5 days/week)</option>
-    <option className="text-black" value="Very active (hard exercise/sports 6-7 days a week)">Very active (hard exercise/sports 6-7 days a week)</option>
-    <option className="text-black" value="Super active (very hard exercise/physical job & exercise 2x/day)">Super active (very hard exercise/physical job & exercise 2x/day)</option>
+    <option value="" className="text-primary text-sm"  disabled>Select your fitness goal</option>
+    <option className="text-black" value="Weight Loss">Weight Loss</option>
+    <option className="text-black" value="Muscle Gain">Muscle Gain</option>
+    <option className="text-black" value="Improved Endurance">Improved Endurance</option>
+    <option className="text-black" value="Overall Fitness">Overall Fitness</option>
+    <option className="text-black" value="Flexibility and Mobility">Flexibility and Mobility</option>
   </select>
 </div>
-    
-         <div className="button flex justify-center my-5">
-            <button onClick={submitInfo} className="px-6 py-2 bg-secondary hover:bg-opacity-70 shadow-lg active:shadow-sm text-xl rounded-md">Submit
 
+         
 
+          <div className="mb-4">
+            <label className="block text-lg mb-2 text-left px-2">
+              Current Activity Level
+            </label>
+            <select
+              name="curActivity"
+              value={formData.curActivity}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border-b-2 outline-none focus:outline-none rounded-lg bg-transparent"
+            >
+              <option className="text-primary text-sm" value="" disabled>
+                Select your activity level
+              </option>
+              <option
+                className="text-black"
+                value="Sedentary (little or no exercise)"
+              >
+                Sedentary (little or no exercise)
+              </option>
+              <option
+                className="text-black"
+                value="Lightly active (light exercise/sports 1-3 days/week)"
+              >
+                Lightly active (light exercise/sports 1-3 days/week)
+              </option>
+              <option
+                className="text-black"
+                value="Moderately active (moderate exercise/sports 3-5 days/week)"
+              >
+                Moderately active (moderate exercise/sports 3-5 days/week)
+              </option>
+              <option
+                className="text-black"
+                value="Very active (hard exercise/sports 6-7 days a week)"
+              >
+                Very active (hard exercise/sports 6-7 days a week)
+              </option>
+              <option
+                className="text-black"
+                value="Super active (very hard exercise/physical job & exercise 2x/day)"
+              >
+                Super active (very hard exercise/physical job & exercise 2x/day)
+              </option>
+            </select>
+          </div>
+
+          <div className="button flex justify-center my-5">
+            <button
+              onClick={submitInfo}
+              className="px-6 py-2 bg-secondary hover:bg-opacity-70 shadow-lg active:shadow-sm text-xl rounded-md"
+            >
+              Submit
             </button>
-         </div>
-    
+          </div>
+
           <div className="bg-primary flex flex-col items-start rounded-lg p-4">
             <h3 className="text-lg font-semibold mb-2">Summary:</h3>
             <p>Name: {formData.name}</p>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,7 @@ import hero1 from '../assets/hero1.jpg';
 const Hero = ({Form:Form}) => {
     
   return(<>
-  <div className="hero relative w-screen overflow-scroll ">
+  <div className="hero relative w-screen overflow-y-scroll">
   <div className="fixed inset-0  bg-center bg-no-repeat bg-cover opacity-40" style={{backgroundImage:`url(${hero1})`, backgroundSize:'100%',backgroundSize:'cover',backgroundPosition:'center',backgroundRepeat:'no-repeat'}}></div>
   <div className="fixed inset-0 bg-gradient-to-r from-primary to-secondary opacity-60"></div>
   <div className="relative lg:px-32  h-full z-10">
